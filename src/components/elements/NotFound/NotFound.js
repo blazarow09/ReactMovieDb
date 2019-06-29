36e65e2a63cf83ac42e11469acbf907560877e93
blazarow09 <blazarow09@gmail.com>
2019-06-29 18:50:24 +0300

    upload NotFound component

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Whoooops. This page does'n exist </h1>
+        </div>
+    )
+}
+
+export default NotFound;
